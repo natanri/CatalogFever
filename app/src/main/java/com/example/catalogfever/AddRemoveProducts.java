cb308dfe0dbfe42e668a64e74b6a64ccbb3159ee
natanri <riosjhonatan44@gmail.com>
2022-03-05 20:47:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/catalogfever/AddRemoveProducts.java
@@ -0,0 +1,14 @@
+package com.example.catalogfever;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddRemoveProducts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_remove_products);
+    }
+}

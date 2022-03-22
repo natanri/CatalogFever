c2110a726063490855989bb7a921d68c75871fc2
natanri <riosjhonatan44@gmail.com>
2022-03-21 20:27:57 -0500

    This code suppose to show us the inventory
    I added a button that will allow us to save product

--- app/src/main/java/com/example/catalogfever/List_perform.java
+++ app/src/main/java/com/example/catalogfever/List_perform.java
@@ -1,14 +1,22 @@
 package com.example.catalogfever;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class List_perform extends AppCompatActivity {
 
     private String number;
     private String product;
-    private String IDnumber;
+    private String ID_number;
 
 
     @Override
@@ -19,13 +27,25 @@ public class List_perform extends AppCompatActivity {
         Intent intent = getIntent();
         number = intent.getStringExtra("number");
         product = intent.getStringExtra("product");
-        IDnumber = intent.getStringExtra("ID");
+        ID_number = intent.getStringExtra("Id_number");
 
-        String inventory = number + product + IDnumber;
+        String inventory = number + product + ID_number;
         Log.d(this.getLocalClassName(), "These are the current stocks " + inventory);
         TextView inventoryObject = findViewById(R.id.tvInventory);
         inventoryObject.setText(inventory);
 
+    }
+
+    public void saveProduct(View view){
+        SharedPreferences sharedPref = getSharedPreferences("Inventory.txt", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putString("number", number);
+        editor.putString("product", product);
+        editor.putString("Id_number", ID_number);
+        editor.apply();
+
+        Toast.makeText(this, "Product Saved", Toast.LENGTH_SHORT).show();
     }
 
 
